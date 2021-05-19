7545adcc0e39b030f6f9b76243634501f6ab783a
linoop k k <linoop1989@gmail.com>
2021-05-19 14:59:04 +0530

    tracking user location in the background

--- app/src/main/java/com/technowaysoftware/mvvmfitness/services/TrackingService.kt
+++ app/src/main/java/com/technowaysoftware/mvvmfitness/services/TrackingService.kt
@@ -1,30 +1,69 @@
 package com.technowaysoftware.mvvmfitness.services
 
+import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.NotificationManager.IMPORTANCE_LOW
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.location.Location
 import android.os.Build
+import android.os.Looper
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.maps.model.LatLng
 import com.technowaysoftware.mvvmfitness.R
 import com.technowaysoftware.mvvmfitness.ui.activities.MainActivity
 import com.technowaysoftware.mvvmfitness.utils.Constants.ACTION_PAUSE_SERVICE
 import com.technowaysoftware.mvvmfitness.utils.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 import com.technowaysoftware.mvvmfitness.utils.Constants.ACTION_START_OR_RESUME_SERVICE
 import com.technowaysoftware.mvvmfitness.utils.Constants.ACTION_STOP_SERVICE
+import com.technowaysoftware.mvvmfitness.utils.Constants.FASTEST_LOCATION_INTERVAL
+import com.technowaysoftware.mvvmfitness.utils.Constants.LOCATION_UPDATE_INTERVAL
+import com.technowaysoftware.mvvmfitness.utils.Constants.MAX_WAIT_TIME
 import com.technowaysoftware.mvvmfitness.utils.Constants.NOTIFICATION_CHANNEL_ID
 import com.technowaysoftware.mvvmfitness.utils.Constants.NOTIFICATION_CHANNEL_NAME
 import com.technowaysoftware.mvvmfitness.utils.Constants.NOTIFICATION_ID
+import com.technowaysoftware.mvvmfitness.utils.TrackingUtility
 import timber.log.Timber
+
+typealias Polyline = MutableList<LatLng>
+typealias PolylineList = MutableList<Polyline>
 
 //We can extend our service form Service or IntentService but now we are extending form LifecycleService
 class TrackingService : LifecycleService() {
 
     var isFirstRun = true
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    companion object {
+        val isTracking = MutableLiveData<Boolean>()
+        val pathPoints = MutableLiveData<PolylineList>()
+    }
+
+    private fun postInitialValues() {
+        isTracking.postValue(false)
+        pathPoints.postValue(mutableListOf())
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        fusedLocationProviderClient = FusedLocationProviderClient(this)
+
+        isTracking.observe(this, Observer {
+            updateLocationTracking(it)
+        })
+    }
+
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         intent?.let {
@@ -33,7 +72,7 @@ class TrackingService : LifecycleService() {
                     if (isFirstRun) {
                         startForegroundService()
                         isFirstRun = false
-                    }else{
+                    } else {
                         Timber.d("Resuming service...")
                     }
                 }
@@ -48,7 +87,62 @@ class TrackingService : LifecycleService() {
         return super.onStartCommand(intent, flags, startId)
     }
 
+    @SuppressLint("MissingPermission")
+    private fun updateLocationTracking(isTracking: Boolean) {
+        if (isTracking) {
+            if (TrackingUtility.hasLocationPermissions(this)) {
+                val request = LocationRequest().apply {
+                    interval = LOCATION_UPDATE_INTERVAL
+                    fastestInterval = FASTEST_LOCATION_INTERVAL
+                    priority = PRIORITY_HIGH_ACCURACY
+
+                }
+                fusedLocationProviderClient.requestLocationUpdates(
+                    request,
+                    locationCallBack,
+                    Looper.getMainLooper()
+                )
+            }
+        } else {
+            fusedLocationProviderClient.removeLocationUpdates(locationCallBack)
+        }
+    }
+
+    val locationCallBack = object : LocationCallback() {
+        override fun onLocationResult(result: LocationResult) {
+            super.onLocationResult(result)
+            if (isTracking.value!!) {
+                result?.locations?.let { locations ->
+                    for (location in locations) {
+                        addPathPoint(location)
+                        Timber.d("New location: ${location.latitude}, ${location.longitude}")
+                    }
+                }
+            }
+        }
+    }
+
+
+    private fun addPathPoint(location: Location?) {
+        location?.let {
+            val position = LatLng(location.latitude, location.longitude)
+            pathPoints.value?.apply {
+                last().add(position)
+                pathPoints.postValue(this)
+            }
+        }
+    }
+
+    private fun addEmptyPolyline() = pathPoints.value?.apply {
+        add(mutableListOf())
+        pathPoints.postValue(this)
+    } ?: pathPoints.postValue(mutableListOf(mutableListOf()))
+
     private fun startForegroundService() {
+
+        addEmptyPolyline()
+
+        isTracking.postValue(true)
 
         val notificationManager =
             getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
--- app/src/main/java/com/technowaysoftware/mvvmfitness/utils/Constants.kt
+++ app/src/main/java/com/technowaysoftware/mvvmfitness/utils/Constants.kt
@@ -14,4 +14,8 @@ object Constants {
     const val NOTIFICATION_CHANNEL_ID = "tracking_channel"
     const val NOTIFICATION_CHANNEL_NAME = "Tracking"
     const val NOTIFICATION_ID = 1//At least one not zero
+
+    const val LOCATION_UPDATE_INTERVAL = 5000L
+    const val FASTEST_LOCATION_INTERVAL = 2000L
+    const val MAX_WAIT_TIME = 1000L
 }
